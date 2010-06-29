395314a584149fa625255d8f7983453f8eaca8dd
gsreynolds <gsreynolds@8c974ef0-c093-640e-c5c6-42294981b572>
2010-06-29 11:14:06 +0000

    Added CEvalxy

--- trunk/modPolyEvalCInterface.f90
+++ trunk/modPolyEvalCInterface.f90
@@ -17,11 +17,9 @@ module modPolyEvalCInterface
     	write(*,*) 'result = ', CtoFtest
     end function CtoFtest
 
-	!Test C interface to Evalx, as opposed to passing a C struct to function
-	!with Fortran derived datatype as argument.
     double precision function CEvalx(n, f, x, type)
     	integer, intent(IN) :: n, type
-    	real(kind=prec), dimension(n) :: f
+    	real(kind=prec), intent(IN), dimension(n) :: f
     	real (kind=prec), intent(IN) :: x
     	type(polynomial) :: poly
 
@@ -55,5 +53,43 @@ module modPolyEvalCInterface
 		end select
 
     end function CEvalx
+
+   	double precision function CEvalxy(n, f, x, y, powers1, powers2, type)
+    	integer, intent(IN) :: n, type
+    	real(kind=prec), intent(IN), dimension(n) :: f
+    	real(kind=prec), intent(IN) :: x, y
+    	real(kind=prec), intent(IN), dimension(n-1) :: powers1, powers2
+    	type(polynomial2) :: poly
+
+		poly%x = x
+		poly%y = y
+		poly%n = n
+		allocate(poly%f(poly%n))
+		poly%f(:) = f(:)
+		allocate(poly%powers(2,poly%n-1))
+		poly%powers(1,:) = powers1(:)
+		poly%powers(2,:) = powers2(:)
+
+		select case (type)
+
+			case (0)
+				write(*,*) 'Evalxy'
+				CEvalxy = Evalxy(poly)
+
+			case (1)
+				write(*,*) 'EvalOptxy'
+				CEvalxy = EvalOptxy(poly)
+
+!			case (2)
+!				write(*,*) 'EvalHorner'
+!				CEvalxy = EvalHornerxy(poly)
+
+			case default
+				write(*,*) 'Invalid type identifier, defaulting to Evalxy'
+				CEvalxy = Evalxy(poly)
+
+		end select
+
+    end function CEvalxy
 
 end module modPolyEvalCInterface
