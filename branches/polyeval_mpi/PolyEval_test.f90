c3a37d4365b9bb9730ce3ced33caac5b88c25591
gsreynolds <gsreynolds@8c974ef0-c093-640e-c5c6-42294981b572>
2010-08-25 12:54:24 +0000

    Added polyeval_mpi branch for comparison with OpenMP implementation.

---
+++ branches/polyeval_mpi/PolyEval_test.f90
@@ -0,0 +1,114 @@
+program PolyEval_test
+	use omp_lib
+	use modPolyEval
+	implicit none
+
+	integer, parameter :: MAXITER = 1!000000000
+	real (kind=prec), parameter :: a = 3.068474, b=0.0d0, c=20.857847, d=0.0d0, e=0.757463, f=0.0d0, g=8.673527
+	real (kind=prec), parameter :: h=765.638467, i=0.0d0, j=-20.889708, k=67.786429, l=-0.754380, m= 1120.000000
+	real (kind=prec) :: x, estrin, horner, brute, brute_multi, bruteopt, bruteopt_multi
+	real (kind=prec) :: startTime, endTime
+	real (kind=prec) :: timeDiff, averageIterTime
+	integer :: loop
+	type(polynomial) :: poly
+	type(polynomial_multi) :: poly_multi
+
+	poly%x = 2.0
+	poly%n = 7
+	allocate(poly%f(poly%n))
+	poly%f = (/a,a,a,a,a,a,a/)
+
+	write(*,*) 'Brute force x'
+
+	startTime = omp_get_wtime()
+	do loop = 1, MAXITER
+		brute = Eval(poly)
+	end do
+	endTime = omp_get_wtime()
+
+	timeDiff = endTime - startTime
+    write(*,*) 'result =', brute
+    write(*,*) 'Completed in', timeDiff
+    write(*,*)
+
+    write(*,*) 'Brute force (optimised) x'
+
+	startTime = omp_get_wtime()
+	do loop = 1, MAXITER
+		bruteopt = EvalOpt(poly)
+	end do
+	endTime = omp_get_wtime()
+
+	timeDiff = endTime - startTime
+    write(*,*) 'result =', bruteopt
+    write(*,*) 'Completed in', timeDiff
+    write(*,*)
+
+    write(*,*) 'Brute force multi'
+
+    poly_multi%m = 2
+	allocate(poly_multi%vars(poly_multi%m))
+    poly_multi%vars(1)=2.0
+    poly_multi%vars(2)=3.0
+    poly_multi%n=poly%n
+    allocate(poly_multi%f(poly_multi%n))
+    allocate(poly_multi%powers(poly_multi%m,poly_multi%n-1))
+    poly_multi%f = poly%f
+	poly_multi%powers(1,:) = (/2,2,1,1,1,0/)
+	poly_multi%powers(2,:) = (/2,1,2,1,0,1/)
+
+	startTime = omp_get_wtime()
+	do loop = 1, MAXITER
+		brute_multi = Eval_multi(poly_multi)
+	end do
+	endTime = omp_get_wtime()
+
+	timeDiff = endTime - startTime
+    write(*,*) 'result =', brute_multi
+    write(*,*) 'Completed in', timeDiff
+    write(*,*)
+
+    write(*,*) 'Brute force multi (optimised)'
+	startTime = omp_get_wtime()
+	do loop = 1, MAXITER
+		bruteopt_multi = EvalOpt_multi(poly_multi)
+	end do
+	endTime = omp_get_wtime()
+
+	timeDiff = endTime - startTime
+    write(*,*) 'result =', bruteopt_multi
+    write(*,*) 'Completed in', timeDiff
+    write(*,*)
+
+	write(*,*) '==============='
+	write(*,*)
+	write(*,*) 'Horners Form'
+
+	startTime = omp_get_wtime()
+	do loop = 1, MAXITER
+		horner = EvalHorner(poly)
+	end do
+	endTime = omp_get_wtime()
+
+	timeDiff = endTime - startTime
+    write(*,*) 'result =', horner
+    write(*,*) 'Completed in', timeDiff
+
+	write(*,*)
+	write(*,*) 'Estrins Method'
+
+	startTime = omp_get_wtime()
+	do loop = 1, MAXITER
+		estrin = EvalEstrin(poly)
+	end do
+	endTime = omp_get_wtime()
+
+	timeDiff = endTime - startTime
+    write(*,*) 'result =', estrin
+	write(*,*) 'Completed in', timeDiff
+
+	write(*,*)
+	write(*,*) 'Horner - Estrin'
+	write(*,*) 'diff   =', horner-estrin
+
+end program PolyEval_test
---
+++ branches/polyeval_mpi/modPolyEval.f90
@@ -0,0 +1,190 @@
+module modPolyEval
+    implicit none
+
+	integer, parameter :: prec = kind(1.0d0)
+
+   	type polynomial
+   		integer :: n
+   		real (kind=prec), allocatable, dimension(:) :: f !Coefficients of polynomial
+		real (kind=prec) :: x !Value of independent variable
+	end type
+
+	type polynomial_multi
+		integer :: n
+		integer :: m
+		real (kind=prec), allocatable, dimension(:) :: f !Coefficients of polynomial
+		real (kind=prec), allocatable, dimension(:) :: vars !Independent variables
+		real (kind=prec), allocatable, dimension(:,:) :: powers
+	end type
+
+	!Evaluate by brute force
+	interface Eval
+		module procedure Eval, Eval_multi
+	end interface
+
+	!Evaluate by optimised brute force method
+	interface EvalOpt
+		module procedure EvalOpt, EvalOpt_multi
+	end interface
+
+    contains
+
+    !Function to evaluate a univariate polynomial by brute force
+   	double precision function Eval(poly)
+   		type(polynomial), intent(IN) :: poly
+   		integer :: i
+   		real (kind=prec), dimension(poly%n) :: monomial
+
+   		do i = 1, poly%n-1
+			monomial(i) = poly%f(i)*poly%x**(poly%n-i)
+   		end do
+
+   		monomial(poly%n) = poly%f(poly%n)
+
+   		Eval = sum(monomial(:))
+
+   	end function Eval
+
+   	 !Function to evaluate a univariate polynomial by brute force, with optimisations
+   	double precision function EvalOpt(poly)
+   		type(polynomial), intent(IN) :: poly
+   		integer :: i, j, numSteps
+   		real (kind=prec), dimension(poly%n) :: monomial
+   		real (kind=prec) :: x2
+
+   		x2 = poly%x*poly%x
+
+   		do i = 1, poly%n-1
+			monomial(i) = poly%f(i)
+			numSteps = (poly%n-i)/2
+			do j = 1, numSteps
+				monomial(i) = monomial(i) * x2
+			end do
+			if (mod(poly%n-i, 2) .ne. 0) then
+				monomial(i) = monomial(i) * poly%x
+   			end if
+   		end do
+
+   		monomial(poly%n) = poly%f(poly%n)
+
+   		EvalOpt = sum(monomial(:))
+
+   	end function EvalOpt
+
+	!Function to evaluate a multivariate polynomial by brute force
+   	double precision function Eval_multi(poly)
+   		type(polynomial_multi), intent(IN) :: poly
+   		integer :: i, j
+   		real (kind=prec), dimension(poly%n) :: monomial
+
+   		do i = 1, poly%n-1
+			monomial(i) = poly%f(i)
+			do j = 1, poly%m
+				monomial(i) = monomial(i)*poly%vars(j)**poly%powers(j,i)
+			end do
+   		end do
+
+   		monomial(poly%n) = poly%f(poly%n)
+
+   		Eval_multi = sum(monomial(:))
+
+   	end function Eval_multi
+
+	!Function to evaluate a multivariate polynomial by brute force, with optimisations
+   	double precision function EvalOpt_multi(poly)
+   		type(polynomial_multi), intent(IN) :: poly
+   		integer :: i, j, k, numSteps
+   		real (kind=prec), dimension(poly%n) :: monomial
+   		real (kind=prec), dimension(poly%m) :: vars2 !variables, squared
+
+   		do i = 1, poly%m
+   			vars2(i) = poly%vars(i)*poly%vars(i)
+   		end do
+
+   		do i = 1, poly%n-1
+			monomial(i) = poly%f(i)
+
+			do j = 1, poly%m
+				numSteps = (poly%powers(j,i))/2
+				do k = 1, numSteps
+					monomial(i) = monomial(i) * vars2(j)
+				end do
+				if (mod(poly%powers(j,i), 2.0d0) .ne. 0) then
+					monomial(i) = monomial(i) * poly%vars(j)
+	   			end if
+   			end do
+   		end do
+
+   		monomial(poly%n) = poly%f(poly%n)
+
+   		EvalOpt_multi = sum(monomial(:))
+
+   	end function EvalOpt_multi
+
+	!Function to evaluate a polynomial using Horner's form
+    double precision function EvalHorner(poly)
+
+		type(polynomial), intent(IN) :: poly
+		integer :: i
+
+		!Evaluate using Horner's Method
+		EvalHorner = poly%f(1)*poly%x
+		do i = 2, poly%n-1
+			EvalHorner = (EvalHorner + poly%f(i))*poly%x
+		end do
+		EvalHorner = EvalHorner + poly%f(poly%n)
+
+	end function EvalHorner
+
+	!Function to evaluate a polynomial using Estrin's method
+	double precision function EvalEstrin(poly)
+		use omp_lib
+
+		type(polynomial), intent(IN) :: poly
+		real (kind=prec), allocatable, dimension(:) :: func, powers
+		real (kind=prec), allocatable, dimension(:,:) :: coeff
+		integer :: i,j,numsteps, shift, nearestpoweroftwo, npow2
+
+		nearestpoweroftwo = 2**ceiling(log(real(poly%n))/log(2.0d0))
+		if (mod(poly%n, nearestpoweroftwo) .ne. 0) then
+			shift = nearestpoweroftwo-mod(poly%n, nearestpoweroftwo)
+			npow2 = poly%n + shift
+		else
+			shift = 0
+			npow2 = poly%n
+		end if
+
+		allocate(func(npow2))
+		func(:) = 0
+		allocate(coeff(npow2, 0:npow2))
+		coeff(:,:) = 0
+
+		numsteps = log(real(npow2))/log(2.0d0)
+		allocate(powers(numsteps))
+
+		func(1+shift:npow2) = poly%f(:)
+
+		!Evaluate using Estrin's Method
+		powers(1) = poly%x
+		do i = 2, numsteps
+			powers(i) = powers(i-1)**2
+		end do
+
+		coeff(:, 0) = func
+
+		do i = 1, numsteps
+
+			!$omp parallel do default(none) shared(coeff, powers, npow2, i) private(j)
+			do j = 1, npow2/(2**i)
+				!write(*,*) 'Thread', OMP_GET_THREAD_NUM(), 'i', i, 'j', j
+				coeff(j, i) = coeff(2*j-1, i-1)*powers(i)+coeff(2*j, i-1)
+			end do
+			!$omp end parallel do
+
+		end do
+
+		EvalEstrin = coeff(1, numsteps)
+
+	end function EvalEstrin
+
+end module modPolyEval
