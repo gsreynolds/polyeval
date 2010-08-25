7b170f0f9f1c09298757797b6296d9323893618a
Gavin Reynolds <gsreynolds@users.noreply.github.com>
2010-08-25 17:56:20 +0000

    Added larger test case. Commented out multivariate code.

--- PolyEval_test.f90
+++ PolyEval_test.f90
@@ -3,7 +3,7 @@ program PolyEval_test
 	use modPolyEval
 	implicit none
 
-	integer, parameter :: MAXITER = 1!000000000
+	integer, parameter :: MAXITER = 1000000!000
 	real (kind=prec), parameter :: a = 3.068474, b=0.0d0, c=20.857847, d=0.0d0, e=0.757463, f=0.0d0, g=8.673527
 	real (kind=prec), parameter :: h=765.638467, i=0.0d0, j=-20.889708, k=67.786429, l=-0.754380, m= 1120.000000
 	real (kind=prec) :: x, estrin, horner, brute, brute_multi, bruteopt, bruteopt_multi
@@ -36,10 +36,14 @@ program PolyEval_test
 	    end if
     end if
 
+	x = 2.0
 	poly%x = 2.0
-	poly%n = 7
+	poly%n = 200
 	allocate(poly%f(poly%n))
-	poly%f = (/a,a,a,a,a,a,a/)
+	poly%f = (/a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a, &
+	& a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a, &
+	& a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a, &
+	& a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a/)
 
 	! Only executing Brute force and Horners form on rank 0
 
@@ -71,42 +75,44 @@ program PolyEval_test
 	    write(*,*) 'Completed in', timeDiff
 	    write(*,*)
 
-	    write(*,*) 'Brute force multi'
-
-	    poly_multi%m = 2
-		allocate(poly_multi%vars(poly_multi%m))
-	    poly_multi%vars(1)=2.0
-	    poly_multi%vars(2)=3.0
-	    poly_multi%n=poly%n
-	    allocate(poly_multi%f(poly_multi%n))
-	    allocate(poly_multi%powers(poly_multi%m,poly_multi%n-1))
-	    poly_multi%f = poly%f
-		poly_multi%powers(1,:) = (/2,2,1,1,1,0/)
-		poly_multi%powers(2,:) = (/2,1,2,1,0,1/)
-
-		startTime = MPI_Wtime()
-		do loop = 1, MAXITER
-			brute_multi = Eval_multi(poly_multi)
-		end do
-		endTime = MPI_Wtime()
-
-		timeDiff = endTime - startTime
-	    write(*,*) 'result =', brute_multi
-	    write(*,*) 'Completed in', timeDiff
-	    write(*,*)
-
-	    write(*,*) 'Brute force multi (optimised)'
-		startTime = MPI_Wtime()
-		do loop = 1, MAXITER
-			bruteopt_multi = EvalOpt_multi(poly_multi)
-		end do
-		endTime = MPI_Wtime()
-
-		timeDiff = endTime - startTime
-	    write(*,*) 'result =', bruteopt_multi
-	    write(*,*) 'Completed in', timeDiff
-	    write(*,*)
-
+	    CALL FLUSH(6)
+!
+!	    write(*,*) 'Brute force multi'
+!
+!	    poly_multi%m = 2
+!		allocate(poly_multi%vars(poly_multi%m))
+!	    poly_multi%vars(1)=2.0
+!	    poly_multi%vars(2)=3.0
+!	    poly_multi%n=poly%n
+!	    allocate(poly_multi%f(poly_multi%n))
+!	    allocate(poly_multi%powers(poly_multi%m,poly_multi%n-1))
+!	    poly_multi%f = poly%f
+!		poly_multi%powers(1,:) = (/2,2,1,1,1,0/)
+!		poly_multi%powers(2,:) = (/2,1,2,1,0,1/)
+!
+!		startTime = MPI_Wtime()
+!		do loop = 1, MAXITER
+!			brute_multi = Eval_multi(poly_multi)
+!		end do
+!		endTime = MPI_Wtime()
+!
+!		timeDiff = endTime - startTime
+!	    write(*,*) 'result =', brute_multi
+!	    write(*,*) 'Completed in', timeDiff
+!	    write(*,*)
+!
+!	    write(*,*) 'Brute force multi (optimised)'
+!		startTime = MPI_Wtime()
+!		do loop = 1, MAXITER
+!			bruteopt_multi = EvalOpt_multi(poly_multi)
+!		end do
+!		endTime = MPI_Wtime()
+!
+!		timeDiff = endTime - startTime
+!	    write(*,*) 'result =', bruteopt_multi
+!	    write(*,*) 'Completed in', timeDiff
+!	    write(*,*)
+!
 		write(*,*) '==============='
 		write(*,*)
 		write(*,*) 'Horners Form'
